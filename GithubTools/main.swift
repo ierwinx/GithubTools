43a69cd70161d569de700bee5adad15cf46c4db0
Eяw!и Gontier <ierwinx@me.com>
2022-09-20 18:57:27 -0500

    se crea herramienta sencilla para crear repo

--- GithubTools/main.swift
+++ GithubTools/main.swift
@@ -1,11 +1,31 @@
-//
-//  main.swift
-//  GithubTools
-//
-//  Created by Erwin Luz León on 20/09/22.
-//
-
 import Foundation
 
-print("Hello, World!")
+func shell(_ command: String) -> String {
+    let task = Process()
+    let pipe = Pipe()
+    task.standardOutput = pipe
+    task.standardError = pipe
+    task.arguments = ["-c", command]
+    task.launchPath = "/bin/zsh"
+    task.launch()
+    
+    let data = pipe.fileHandleForReading.readDataToEndOfFile()
+    guard let output = String(data: data, encoding: .utf8) else {
+        return ""
+    }
+    return output
+}
 
+func readVariables() {
+    print("⭐️ Github tools v1.0 ⭐️")
+    print("Escribe el nombre del Repositorio -> ")
+    guard let strRepo = readLine(), !strRepo.isEmpty else { return }
+    print("Escribe una descripción -> ")
+    guard let strDescription = readLine(), !strDescription.isEmpty else { return }
+    
+    print(shell("gh repo create \(strRepo) --public -d \"\(strDescription)\""))
+    print(shell("git remote add origin https://github.com/ierwinx/\(strRepo).git"))
+    print(shell("git push -u origin main"))
+}
+
+readVariables()
